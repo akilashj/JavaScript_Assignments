f0abb166d68cc1f029c7bde8b1eb98f791f203bc
akilashj <shashiduniakila98@outlook.com>
2025-11-11 16:36:41 +0200

    Assignment  7 - a program that asks the user for an integer and tells if the number is a prime number.

--- Module01/Assignment9.js
+++ Module01/Assignment9.js
@@ -3,3 +3,15 @@ use strict';
 const number = parseInt(prompt('Enter an integer:'));
 
 let primenumber = true;
+if (number <= 1) {
+  primenumber = false;
+}
+
+else {
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      primenumber = false;
+      break;
+    }
+  }
+}
