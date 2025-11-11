d9ad9df1ed9e2030f539f7be711e4219532d69ec
akilashj <shashiduniakila98@outlook.com>
2025-11-11 18:25:26 +0200

    Assignment  3 - a program that asks for the names of six dogs and print them to an unordered list.

--- Module02/Assignment3.js
+++ Module02/Assignment3.js
@@ -1,3 +1,10 @@
 'use Strict';
 function getNames() {
   const dogs = [];
+
+for (let i = 0; i < 6; i++) {
+    const name = prompt(`Enter the name of the dog ${i + 1}:`);
+    dogs.push(name.trim());
+  }
+
+dogs.sort().reverse();
