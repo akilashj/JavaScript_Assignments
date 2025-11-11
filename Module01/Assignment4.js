de58a5d7e59f720de9acc506f234393ea09be8ff
akilashj <shashiduniakila98@outlook.com>
2025-11-11 15:05:10 +0200

    Assignment  4 - an electronic sorting hat that asks for a student's name and draws a room for that student.

--- Module01/Assignment4.js
+++ Module01/Assignment4.js
@@ -3,3 +3,20 @@ const studentname = prompt( 'Enter your good name, young wizard:');
 const randomnumber = Math.floor(Math.random() *4 + 1);
 
 let house
+
+if
+(randomnumber == 1){
+house = "Gryffindor";
+}
+
+else if
+(randomnumber == 2){
+house = "Slytherin";
+}
+else if
+(randomnumber == 3){
+house = "Hufflepuff";
+}
+else{
+house = "Ravenclaw";
+}
