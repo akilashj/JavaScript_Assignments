87fbe3f0f586087d8172e830a80ae77b84b0acef
akilashj <shashiduniakila98@outlook.com>
2025-11-11 16:21:07 +0200

    Assignment  7 - a program that olls user defined number of dice and displays the sum of the results of the dice rolls.

--- Module01/Assignment7.js
+++ Module01/Assignment7.js
@@ -3,3 +3,13 @@
 const number = parseInt(prompt('Enter the count of dice you would like to roll: '));
 
 let sum = 0;
+
+for
+(let i = 0; i < number; i++) {
+const roll = Math.floor(Math.random() * 6) + 1;
+  sum += roll;
+}
+
+document.querySelector('#output').innerHTML = `
+  You rolled ${number} dice. <br>
+  The total sum is ${sum}.`;
