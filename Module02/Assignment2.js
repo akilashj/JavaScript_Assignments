9be517cb05addbfad1962b0de32d276bf9dc49b2
akilashj <shashiduniakila98@outlook.com>
2025-11-11 17:49:07 +0200

    Assignment  2 - a program that prompts the user for asks the user for the number of participants then the names of all participants and finally prints the names of the participants on the web page in an ordered list.

--- Module02/Assignment2.js
+++ Module02/Assignment2.js
@@ -1,7 +1,14 @@
 function Participants() {
   const number = parseInt(prompt("Enter the number of participants:"));
 
-  if (isNaN(number) || number <= 0) {
+  if ((number) || number <= 0) {
     alert("Please Enter a valid number!");
     return;
+  }
+
+  const participants = [];
+
+  for (let i = 0; i < number; i++) {
+    const name = prompt(`Enter the name of participant ${i + 1}:`);
+    participants.push(name.trim());
   }
