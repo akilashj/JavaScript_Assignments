5fcc5c46bb3e523c22b06fb765d5344ae2e3a6e0
akilashj <shashiduniakila98@outlook.com>
2025-11-11 15:38:58 +0200

    Assignment  4 - an electronic sorting hat that asks for a student's name and draws a room for that student.

--- Module01/Assignment5.js
+++ Module01/Assignment5.js
@@ -3,7 +3,7 @@
 const year = parseInt(prompt('Enter a year:'));
 
 let result = '';
-if ((year / 4 == 0 && year / 100! == 0 ) || (year / 400 == 0)){
+if ((year / 4 == 0 && year / 100 == 0 ) || (year / 400 == 0)){
 result = `${year} is a Leap Year!`;
 }
 
