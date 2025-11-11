1d7169c840ce47aa1caa6a2c3b30d2910f758b8b
akilashj <shashiduniakila98@outlook.com>
2025-11-11 17:05:59 +0200

    Assignment  7 - a program that prints all leap years from the interval given by the user.

--- Module01/Assignment8.js
+++ Module01/Assignment8.js
@@ -2,11 +2,20 @@
 const Start = parseInt(prompt('Enter the start year:'));
 const End = parseInt(prompt('Enter the end year:'));
 
-let leapYears = [];
+let leapyears;
 for
  (let year = Start; year <= End; year++) {
   if ((year % 4 == 0 && year % 100 !== 0) || (year % 400 == 0)) {
-    leapYears.push(year);
+    leapyears.push(year);
   }
 }
+
+let output = '<ul>';
+for (let i = 0; i < leapyears.length; i++) {
+  output += `<li>${leapyears[i]}</li>`;
+}
+
+output += '</ul>';
+document.querySelector('#result').innerHTML = `
+  Leap years between ${Start} and ${End}:${output}`;
 
