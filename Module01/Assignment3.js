caf02763f9484348f303d26d4ac93b5db48f4304
akilashj <shashiduniakila98@outlook.com>
2025-11-11 14:30:43 +0200

    Assignment  3 - a program that prompts for three integers and then prints the sum, product and average of the numbers.

--- Module01/Assignment3.js
+++ Module01/Assignment3.js
@@ -1,11 +1,13 @@
 'use strict';
-let num1 = parseInt(prompt('Enter First Number:'));
-let num2 = parseInt(prompt('Enter Second Number:'));
-let num3 = ParseInt(prompt('Enter Third Number:'));
+const num1 = parseInt(prompt('Enter the First integer:'));
+const num2 = parseInt(prompt('Enter the Second integer:'));
+const num3 = parseInt(prompt('Enter the Third integer:'));
 
-let sum = num1 + num2 + num3;
-let product = num1 * num2 * num3
-let average = sum / 3
-document.querySelector('#result').innerHTML =`Sum of the numbers is, ${sum};
-document.querySelector('#result').innerHTML =`Product of the numbers is, ${product};
-document.querySelector('#result').innerHTML =`Average of the numbers is, ${average};
+const sum = num1 + num2 + num3;
+const product = num1 * num2 * num3;
+const average = sum / 3;
+
+document.querySelector('#result').innerHTML = `
+  Sum: ${sum}
+  Product: ${product}
+  Average: ${average.toFixed(2)}`;
