55b324098f94553481dfc3645b1478fa9f89b3ae
akilashj <shashiduniakila98@outlook.com>
2025-11-11 18:37:12 +0200

    Assignment  4 - a program that asks the user for numbers and then printed in the console from the largest to the smallest.

--- Module02/Assignment4.js
+++ Module02/Assignment4.js
@@ -1,3 +1,17 @@
 'use strict';
 function sortNumbers() {
   const numbers = [];
+
+  while (true) {
+    const input = prompt("Enter a Number:");
+    const num = Number(input);
+
+    if (num == 0) {
+      break;
+    }
+    if (!isNaN(num)) {
+      numbers.push(num);
+    } else {
+      alert("Please enter a valid number!");
+    }
+  }
