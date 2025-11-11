d4a1de7a57b1f6f2d78eab3a4440f1009cb4df87
akilashj <shashiduniakila98@outlook.com>
2025-11-11 19:45:03 +0200

    Assignment  7 - a program that get the number of sides on the dice as a parameter and it continues until the program gets the maximum number on the dice, which is asked from the user at the beginning.

--- Module02/Assignment7.js
+++ Module02/Assignment7.js
@@ -7,9 +7,9 @@ function startRolling() {
   const sides = parseInt(prompt("Enter the number of sides on your dice:"));
 
   if (isNaN(sides) || sides < 2) {
-    alert("Please enter a valid number (at least 2).");
+    alert("Please enter a valid number!");
     return;
   }
 
   let roll;
-  let output = `<h3>Rolling a ${sides}-sided dice...</h3><ul>`;
+  let output = `Rolling a ${sides}-sided dice...<ul>`;
