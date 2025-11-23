cd222d754c9e5234470bdf94a306981824d72bd7
akilashj <shashiduniakila98@outlook.com>
2025-11-23 15:30:19 +0200

    task 3.

--- Module03/t3/3.js
+++ Module03/t3/3.js
@@ -1,2 +1,13 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+
+const target = document.getElementById('target');
+
+let htmlContent = '';
+
+for (let i = 0; i < names.length; i++) {
+    htmlContent += `<li>${names[i]}</li>`;
+}
+target.innerHTML = htmlContent;
+
+
